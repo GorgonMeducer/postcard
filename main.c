d97684e8ede523064a8683ca29a7ceb4696df1c8
Gabriel Wang <embedded_zhuoran@Hotmail.com>
2025-02-25 18:33:57 +0000

    Update main.c

--- main.c
+++ main.c
@@ -235,12 +235,13 @@ static void print_help(void)
 {
     printf("The Postcard is a command line tool that generates and prints out a postcard with given input picture and a story text. \r\n");
     printf("\r\noptions:\r\n");
-    printf("\t-h, --help         show this help message and exit\r\n");
-    printf("\t-p [picture path]  Input picture (*.bmp)\r\n");
-    printf("\t-t [text path]     Input text file.\r\n");
-    printf("\t--A4, --a4         Use A4 papers rather than A5 papers for printing.\r\n");
-    printf("\t--dryrun           Generate PDF and skip printing.\r\n");
-    printf("\t--no_bg_color      No background colour for the story board.\r\n");
+    printf("\t-h, --help                show this help message and exit\r\n");
+    printf("\t-p <picture path>         Input picture (*.bmp)\r\n");
+    printf("\t-t <text path>            Input text file.\r\n");
+    printf("\t-ratio <scaling ratio>    Font size scaling ratio in floating point, e.g -ratio 0.95 \r\n");
+    printf("\t--A4, --a4                Use A4 papers rather than A5 papers for printing.\r\n");
+    printf("\t--dryrun                  Generate PDF and skip printing.\r\n");
+    printf("\t--no_bg_color             No background colour for the story board.\r\n");
     printf("\r\n");
 }
 
